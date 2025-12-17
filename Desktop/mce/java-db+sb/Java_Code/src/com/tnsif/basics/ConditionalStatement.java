3b552dd26d000a7371cab1ee26eb95c510f83595
priyadharshini2084 <priyadhasinipriya@gmail.com>
2025-12-17 11:15:46 +0530

    conditional statement

---
+++ Desktop/mce/java-db+sb/Java_Code/src/com/tnsif/basics/ConditionalStatement.java
@@ -0,0 +1,14 @@
+package com.tnsif.basics;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		for (int i = 1; i<=1000 ; i++)
+		{
+			System.out.println("Value of i:");
+			System.out.println(i);
+		}
+
+	}
+
+}
