8ab6b61a765101eb389bd3e7e30666e0cc7b8cff
priyadharshini2084 <priyadhasinipriya@gmail.com>
2025-12-30 11:18:09 +0530

    Multithreading_Synchronization

--- tnsif.javaprograms/src/com/tnsif/multithreadingday2/MyChildThread.java
+++ tnsif.javaprograms/src/com/tnsif/multithreadingday2/MyChildThread.java
@@ -1,5 +1,21 @@
+//Program to define ChildThread 
 package com.tnsif.multithreadingday2;
 
-public class MyChildThread {
+public class MyChildThread extends Thread {
+	@Override
+	public void run() {
+		System.out.println("Inside run() "
+				+ "Thread is alive or not? " + this.isAlive());
+		int no = 0;
+		while (no < 4) {
+			no++;
+			System.out.println("number = " + no);
+			try {
+				sleep(500);
 
+			} catch (InterruptedException exp) {
+				System.err.println("Thread Interrupted ...");
+			}
+		}
+	}
 }
