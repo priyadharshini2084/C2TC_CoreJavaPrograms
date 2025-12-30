384643e67b7c750427274afa27f92b86d5d93180
priyadharshini2084 <priyadhasinipriya@gmail.com>
2025-12-30 10:53:58 +0530

    Wrapper class

---
+++ tnsif.javaprograms/src/com/tnsif/wrapper/WrapperClassDemo.java
@@ -0,0 +1,23 @@
+package com.tnsif.wrapper;
+
+public class WrapperClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int x = 5;
+		
+		Integer y = Integer.valueOf(x);
+		
+		
+		
+		int z = y;
+		
+		System.out.println(y +" "+ z);
+
+	}
+
+}
